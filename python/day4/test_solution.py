617860c5c19cca6319c1e1fb4970750de7a9d507
Kris Lemaire <kris.lemaire@dbxprt.com>
2022-12-04 12:06:17 +0100

    Improved tests : - make test for partial and full overlap generic - cover more overlap-cases

--- python/day4/test_solution.py
+++ python/day4/test_solution.py
@@ -14,27 +14,33 @@ def test_read_data():
                     ['6-6', '4-6'],
                     ['2-6', '4-8'],]
 
-@pytest.mark.parametrize('assign1, assign2, result',
-                         [['2-4', '6-8', False],
-                          ['2-3', '4-5', False],
-                          ['5-7', '7-9', False],
-                          ['2-8', '3-7', True],
-                          ['6-6', '4-6', True],
-                          ['2-6', '4-8', False]])
-def test_assignments_full_overlap(assign1, assign2, result):
-    assert Assignement.is_full_overlap(Assignement(assign1), Assignement(assign2)) == result
+# 'assign1, assign2, partial_overlap, full_overlap'
+test_data = [['2-4', '6-8', False, False], # no overlap with place between
+             ['6-8', '2-4', False, False],
+             ['2-3', '4-5', False, False], # no overlap with no place between
+             ['4-5', '2-3', False, False],
+             ['5-7', '7-9', True, False],  # partial overlap with just 1 section
+             ['7-9', '5-7', True, False],
+             ['2-6', '4-8', True, False],  # partial overlap with multiple sections
+             ['4-8', '2-6', True, False],
+             ['2-8', '3-7', True, True],   # full overlap
+             ['3-7', '2-8', True, True],
+             ['6-6', '4-6', True, True],   # full overlap of section with a singleton
+             ['4-4', '4-6', True, True],
+             ['4-6', '6-6', True, True],
+             ['4-6', '4-4', True, True],
+             ['6-6', '6-6', True, True],  # Full overlap of 2 singletons 
+             ['1-3', '1-3', True, True],  # Full overlap of 2 exact sections
+             ]
+@pytest.mark.parametrize('assign1, assign2, partial_overlap, full_overlap',
+                         test_data)
+def test_assignments_full_overlap(assign1, assign2, partial_overlap, full_overlap):
+    assert Assignement.is_full_overlap(Assignement(assign1), Assignement(assign2)) == full_overlap
 
-@pytest.mark.parametrize('assign1, assign2, result',
-                         [['2-4', '6-8', False],
-                          ['2-3', '4-5', False],
-                          ['6-8', '2-4', False],
-                          ['4-5', '2-3', False],
-                          ['5-7', '7-9', True],
-                          ['2-8', '3-7', True],
-                          ['6-6', '4-6', True],
-                          ['2-6', '4-8', True]])
-def test_assignments_partial_overlap(assign1, assign2, result):
-    assert Assignement.is_partial_overlap(Assignement(assign1), Assignement(assign2)) == result
+@pytest.mark.parametrize('assign1, assign2, partial_overlap, full_overlap',
+                         test_data)
+def test_assignments_partial_overlap(assign1, assign2, partial_overlap, full_overlap):
+    assert Assignement.is_partial_overlap(Assignement(assign1), Assignement(assign2)) == partial_overlap
 
 # Global tests
 def test_test_input_file_solution1():
