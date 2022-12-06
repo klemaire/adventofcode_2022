8e4ad4360b892ab88cbf3993fcbe64202a98020b
Kris Lemaire <kris.lemaire@dbxprt.com>
2022-12-06 21:24:51 +0100

    Add solution2 day6

--- python/day6/solution.py
+++ python/day6/solution.py
@@ -1,20 +1,31 @@
 TEST_FILE = "../../data/day6/test_input"
+TEST_FILE2 = "../../data/day6/test_input2"
 INPUT_FILE = "../../data/day6/input"
+
 
 def read_data(file):
     return open(file).read().strip()
 
-def solution1(input):
-    for i in range(len(input) - 3):
-        section = input[i:i+4]
-        if len({letter:'' for letter in section}) == 4:
-            return i + 4
+
+def get_location_of_unique_string(data_stream, length_marker=4):
+    for i in range(len(data_stream) - (length_marker - 1)):
+        section = data_stream[i : i + length_marker]
+        if len({letter: "" for letter in section}) == length_marker:
+            return i + length_marker
     return -1
 
 
-def solution2(input):
-    None
+def solution1(file):
+    return get_location_of_unique_string(read_data(file))
 
-if __name__ == '__main__':
-    print("test = ", solution1(read_data(TEST_FILE)))
-    print("test_input = ", solution1(read_data(INPUT_FILE)))
+
+def solution2(file):
+    return get_location_of_unique_string(read_data(file), length_marker=14)
+
+
+if __name__ == "__main__":
+    print("test = ", solution1(TEST_FILE))
+    print("test_input = ", solution1(INPUT_FILE))
+
+    print("test = ", solution2(TEST_FILE2))
+    print("test_input = ", solution2(INPUT_FILE))
--- python/day6/test_solution.py
+++ python/day6/test_solution.py
@@ -1,17 +1,53 @@
 import pytest
 from solution import (
+    TEST_FILE,
+    TEST_FILE2,
+    INPUT_FILE,
+    get_location_of_unique_string,
     solution1,
-    solution2
+    solution2,
 )
 
-@pytest.mark.parametrize('input, expected_result',
+
+@pytest.mark.parametrize(
+    "input, expected_result",
     [
         ["bvwbjplbgvbhsrlpgdmjqwftvncz", 5],
         ["nppdvjthqldpwncqszvftbrmjlhg", 6],
         ["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10],
         ["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11],
-    ]
+    ],
 )
-def test_solution1(input, expected_result):
-    assert solution1('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10
+def test_get_location_of_unique_string_solution1(input, expected_result):
+    assert get_location_of_unique_string(input) == expected_result
 
+
+@pytest.mark.parametrize(
+    "input, expected_result",
+    [
+        ["mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19],
+        ["bvwbjplbgvbhsrlpgdmjqwftvncz", 23],
+        ["nppdvjthqldpwncqszvftbrmjlhg", 23],
+        ["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29],
+        ["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26],
+    ],
+)
+def test_get_location_of_unique_string_solution2(input, expected_result):
+    assert get_location_of_unique_string(input, length_marker=14) == expected_result
+
+
+# Global tests
+def test_test_input_file_solution1():
+    assert solution1(TEST_FILE) == 10
+
+
+def test_input_file_solution1():
+    assert solution1(INPUT_FILE) == 1804
+
+
+def test_test_input_file_solution2():
+    assert solution2(TEST_FILE2) == 29
+
+
+def test_input_file_solution2():
+    assert solution2(INPUT_FILE) == 2508
